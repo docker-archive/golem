b680ac3e2ad14e6261665d93537230dba4884dc3
Derek McGowan <derek@mcgstyle.net>
2015-08-17 17:38:41 -0700

    Add helper profile functions for Docker development flow
    Signed-off-by: Derek McGowan <derek@mcgstyle.net> (github: dmcgowan)

---
+++ scripts/profile.sh
@@ -0,0 +1,33 @@
+# golem is an function to run tests from a local gopath
+# The local gopath should have docker and golem checked out
+function golem() {
+  $GOPATH/src/github.com/dmcgowan/golem/run.sh $@
+}
+
+# golem-docker is a function to run tests for a Docker development build
+# The first argument is docker binary bundle version to run ("default" to use image's default)
+# First issue "make binary" for the version to run
+function golem-docker() {
+  if [ "$1" != "default" ]; then
+    DOCKER_BINARY=$GOPATH/src/github.com/docker/docker/bundles/$1/binary/docker
+    if [ ! -f $DOCKER_BINARY ]; then
+      current_version=`cat $GOPATH/src/github.com/docker/docker/VERSION`
+      echo "$DOCKER_BINARY does not exist"
+      echo "Current checked out docker version: $current_version"
+      echo "Checkout desired version and run 'make binary' from $GOPATH/src/github.com/docker/docker"
+      return 1
+    fi
+  fi
+  shift
+
+  DOCKER_BINARY="$DOCKER_BINARY" $GOPATH/src/github.com/dmcgowan/golem/run.sh $@
+}
+
+# golem-docker-dev is a function to run tests on active docker development directory
+function golem-docker-dev() {
+  pushd $GOPATH/src/github.com/docker/docker/
+  make binary
+  version=`cat VERSION`
+  golem-docker $version $@
+}
+
