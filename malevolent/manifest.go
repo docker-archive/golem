222d929168563286d4eb311ce3288b3f0a697a1e
Derek McGowan <derek@mcgstyle.net>
2015-11-06 16:31:34 -0800

    Test image poisoning and id alteration
    Signed-off-by: Derek McGowan <derek@mcgstyle.net> (github: dmcgowan)

--- malevolent/manifest.go
+++ malevolent/manifest.go
@@ -6,11 +6,19 @@ import (
 	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"regexp"
 	"strconv"
+	"strings"
 
 	"github.com/Sirupsen/logrus"
 	"github.com/docker/distribution/digest"
 	"github.com/docker/distribution/manifest"
+	"github.com/gorilla/mux"
+)
+
+var (
+	idRegexp     = regexp.MustCompile(`[0-9a-f]{64}`)
+	idLineRegexp = regexp.MustCompile(`{\"id\":\"([0-9a-f]{64})\",`)
 )
 
 type manifestChanger struct {
@@ -54,6 +62,48 @@ func (m manifestChanger) alterManifest(rw http.ResponseWriter, r *http.Request) 
 	}
 }
 
+func signAndServeManifest(rw http.ResponseWriter, code int, header http.Header, m *manifest.Manifest) {
+	newSm, err := manifest.Sign(m, key)
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if header.Get("Docker-Content-Digest") != "" {
+		payload, err := newSm.Payload()
+		if err != nil {
+			http.Error(rw, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		dgst, err := digest.FromBytes(payload)
+		if err != nil {
+			http.Error(rw, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		header.Set("Docker-Content-Digest", dgst.String())
+	}
+
+	copied, err := json.MarshalIndent(newSm, "", "   ")
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	header.Set("Content-Length", strconv.Itoa(len(copied)))
+	copyHeader(rw.Header(), header)
+	rw.WriteHeader(code)
+
+	n, err := rw.Write(copied)
+	if err != nil {
+		logrus.Errorf("Error writing: %s", err)
+		return
+	}
+	if n != len(copied) {
+		logrus.Errorf("Short write: wrote %d, expected %d", n, len(copied))
+	}
+}
+
 // rename changes the name in a manifest and re-signs with a different key
 func (m manifestChanger) rename(rw http.ResponseWriter, r *http.Request, newName string) {
 	recorder := httptest.NewRecorder()
@@ -70,53 +120,46 @@ func (m manifestChanger) rename(rw http.ResponseWriter, r *http.Request, newName
 
 	sm.Manifest.Name = newName
 
-	newSm, err := manifest.Sign(&sm.Manifest, key)
-	if err != nil {
-		http.Error(rw, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	if recorder.Header().Get("Docker-Content-Digest") != "" {
-		payload, err := newSm.Payload()
-		if err != nil {
-			http.Error(rw, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		dgst, err := digest.FromBytes(payload)
-		if err != nil {
-			http.Error(rw, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		recorder.Header().Set("Docker-Content-Digest", dgst.String())
-	}
-
-	copied, err := json.MarshalIndent(newSm, "", "   ")
-	if err != nil {
-		http.Error(rw, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	recorder.Header().Set("Content-Length", strconv.Itoa(len(copied)))
-	copyHeader(rw.Header(), recorder.Header())
-	rw.WriteHeader(recorder.Code)
-
-	n, err := rw.Write(copied)
-	if err != nil {
-		logrus.Errorf("Error writing: %s", err)
-		return
-	}
-	if n != len(copied) {
-		logrus.Errorf("Short write: wrote %d, expected %d", n, len(copied))
-	}
+	signAndServeManifest(rw, recorder.Code, recorder.Header(), &sm.Manifest)
 }
 
-// badRemoteDigest
-// stripSignature
+// poison renames the image ID in the v1 history to the sha256 of the seed
+func (m manifestChanger) poison(rw http.ResponseWriter, r *http.Request, replaceID string) {
+	recorder := httptest.NewRecorder()
 
-// changeSignature
+	m.Handler.ServeHTTP(recorder, r)
 
-// addSignature
+	b := recorder.Body.Bytes()
+
+	var sm manifest.SignedManifest
+	if err := json.Unmarshal(b, &sm); err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if len(sm.Manifest.History) == 0 {
+		http.Error(rw, "invalid manifest", http.StatusInternalServerError)
+		return
+	}
+
+	if idRegexp.MatchString(replaceID) {
+		http.Error(rw, "invalid v1 id, 64 character hex required", http.StatusInternalServerError)
+		return
+	}
+
+	// Find ID to replace from first element
+	matches := idLineRegexp.FindStringSubmatch(sm.Manifest.History[0].V1Compatibility)
+	if len(matches) != 2 {
+		http.Error(rw, "could not find id in history", http.StatusInternalServerError)
+		return
+	}
+
+	for i, history := range sm.Manifest.History {
+		sm.Manifest.History[i].V1Compatibility = strings.Replace(history.V1Compatibility, matches[1], replaceID, -1)
+	}
+
+	signAndServeManifest(rw, recorder.Code, recorder.Header(), &sm.Manifest)
+}
 
 func (m manifestChanger) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	if r.Method != "GET" {
@@ -130,6 +173,14 @@ func (m manifestChanger) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 		m.rename(rw, r, "newname")
 	case "badsignature":
 		m.alterManifest(rw, r)
+	case "poison":
+		vars := mux.Vars(r)
+		ref := vars["ref"]
+		if ref == "" {
+			logrus.Infof("No tag provided to use as poison seed")
+			m.Handler.ServeHTTP(rw, r)
+		}
+		m.poison(rw, r, ref)
 	default:
 		logrus.Infof("No manifest operation for %q, passing through", operation)
 		m.Handler.ServeHTTP(rw, r)
--- registry/malevolent.bats
+++ registry/malevolent.bats
@@ -22,6 +22,8 @@ function setup() {
 }
 
 @test "Test malevolent bad signature change" {
+	# Only skip after 1.8.3
+	skip "Bad signature no longer enforced"
 	docker tag -f $base:latest $host/$base/badsignature:latest
 	run docker push $host/$base/badsignature:latest
 	[ "$status" -eq 0 ]
@@ -40,7 +42,7 @@ function setup() {
 	has_digest "$output"
 
 	# Pull attempt should fail to verify manifest digest
-	run docker pull "$image@$digest"
+	run docker pull "$imagename@$digest"
 	[ "$status" -ne 0 ]
 }
 
@@ -77,3 +79,94 @@ function setup() {
 	[ "$status" -ne 0 ]
 }
 
+@test "Test malevolent poisoned images" {
+        truncid="777cf9284131"
+	poison="${truncid}d77ca0863fb7f054c0a276d7e227b5e9a5d62b497979a481fa32"
+	image1="$host/$base/image1/poison:$poison"
+	tempImage $image1
+	run docker push $image1
+	echo "$output"
+	[ "$status" -eq 0 ]
+	has_digest "$output"
+
+	image2="$host/$base/image2/poison:$poison"
+	tempImage $image2
+	run docker push $image2
+	echo "$output"
+	[ "$status" -eq 0 ]
+	has_digest "$output"
+
+
+	# Remove image to ensure layer is pulled and digest verified
+	docker rmi -f $image1
+	docker rmi -f $image2
+
+	run docker pull $image1
+	echo "$output"
+	[ "$status" -eq 0 ]
+	run docker pull $image2
+	echo "$output"
+	[ "$status" -eq 0 ]
+
+	# Test if there are multiple images
+	run docker images
+	echo "$output"
+	[ "$status" -eq 0 ]
+
+	# Test images have same ID and not the poison
+	id1=$(docker images -q $image1)
+	id2=$(docker images -q $image2)
+
+	[ "$id1" != "$id2" ]
+
+	[ "$id1" != "$truncid" ]
+
+	[ "$id2" != "$truncid" ]
+}
+
+@test "Test malevolent altered identical images" {
+        truncid1="777cf9284131"
+	poison1="${truncid1}d77ca0863fb7f054c0a276d7e227b5e9a5d62b497979a481fa32"
+        truncid2="888cf9284131"
+	poison2="${truncid2}d77ca0863fb7f054c0a276d7e227b5e9a5d62b497979a481fa64"
+	image1="$host/$base/image1/poison:$poison1"
+	tempImage $image1
+	run docker push $image1
+	echo "$output"
+	[ "$status" -eq 0 ]
+	has_digest "$output"
+
+	image2="$host/$base/image2/poison:$poison2"
+	docker tag $image1 $image2
+	run docker push $image2
+	echo "$output"
+	[ "$status" -eq 0 ]
+	has_digest "$output"
+
+
+	# Remove image to ensure layer is pulled and digest verified
+	docker rmi -f $image1
+	docker rmi -f $image2
+
+	run docker pull $image1
+	echo "$output"
+	[ "$status" -eq 0 ]
+	run docker pull $image2
+	echo "$output"
+	[ "$status" -eq 0 ]
+
+	# Test if there are multiple images
+	run docker images
+	echo "$output"
+	[ "$status" -eq 0 ]
+
+	# Test images have same ID and not the poison
+	id1=$(docker images -q $image1)
+	id2=$(docker images -q $image2)
+
+	[ "$id1" == "$id2" ]
+
+	[ "$id1" != "$truncid1" ]
+
+	[ "$id2" != "$truncid2" ]
+}
